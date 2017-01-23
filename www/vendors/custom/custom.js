aa126d1483c36b976a17cd8ef4f13b19d69d2160
Hasan KAYA <kaya.hasn@gmail.com>
2017-01-24 00:48:10 +0300

    develop

---
+++ www/vendors/custom/custom.js
@@ -0,0 +1,61 @@
+$('.aracekbilgi').hide();
+$('.aracyakitbilgi').hide();
+$('.flat').on('ifClicked', function (event) {
+	if (this.value == '3') {
+		$('.aracyakitbilgi').show();
+	}else{
+		$('.aracyakitbilgi').hide();
+	}
+});
+
+
+//Sunucuya Gönder...
+function AracSorgula(){
+	var plaka = $("#AracPlaka").val();
+	var harekettipi = $(".flat:checked").val();
+	var yakitmiktar = $("#yakitmiktar").val();
+	var kullanici = localStorage.kullanici;
+
+	$.ajax(
+	{       
+		type: "GET",
+		url:  "http://www.giltas.com.tr/gats/test.php",
+		data : {plaka: plaka, 
+			harekettipi : harekettipi, 
+			yakitmiktar : yakitmiktar, 
+			kullanici : kullanici},
+			success: function(sonuc){
+				sonuc = JSON.parse(sonuc);
+				$('.response-sonuc .alert').addClass(sonuc.code);
+				$('.response-sonuc .alert').html(sonuc.message);
+				$('.barcodebutton').hide();
+				$('.aracekbilgi').hide();
+
+			}
+		})
+} 
+
+//Barcode Runner
+function BarcodeRunner(){ 
+	cordova.plugins.barcodeScanner.scan(
+		function (result) {
+			$('.aracekbilgi').show();
+			$('#AracPlaka').val(result.text);
+			$('#islemzamani').html(tarih.getDate()+' '+aylar[ay]+' '+yil+' '+gunler[gun]+' '+h+ ":" +m);  
+		},
+		function (error) {
+			alert("Scanning failed: " + error);
+		},
+		{
+			preferFrontCamera : false, // iOS and Android
+			showFlipCameraButton : false, // iOS and Android
+			showTorchButton : true, // iOS and Android
+			torchOn: true, // Android, launch with the torch switched on (if available)
+			prompt : "Place a barcode inside the scan area", // Android
+			resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+			formats : "QR_CODE", // default: all but PDF_417 and RSS_EXPANDED
+			orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+			disableAnimations : true // iOS
+		}
+	);
+}         
